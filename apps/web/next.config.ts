9b956121da21b26cc7384972e2faa24e59d4de8c
Kazi Tanvirul Islam Sakib <ktisakib@gmail.com>
2025-11-09 19:35:48 +0000

    feat(docker): add reset script for container and data cleanup
    feat(docker): create Kong configuration for API routes and access control
    
    feat(docker): initialize Supabase database with SQL scripts
    
    feat(docker): implement JWT configuration for database
    
    feat(docker): set up logging schema and roles for analytics
    
    feat(docker): add webhooks and functions for Supabase
    
    feat(docker): configure Vector for log management and routing
    
    feat(docker): implement connection pooling with Supavisor

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,13 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  transpilePackages: [
-    "@workspace/ui",
-    "@workspace/database",
-    "@workspace/transactional",
-  ],
-  output: "standalone",
+    /* config options here */
+    logging: {
+        fetches: {
+            fullUrl: true
+        }
+    },
+    transpilePackages: [
+        "@workspace/ui",
+        "@workspace/database",
+        "@workspace/transactional",
+    ],
+    output: "standalone",
 };
 
 export default nextConfig;
